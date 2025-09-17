06eb60243bf0c06975ff46abc2975f3a9cd41cdd
minji choi <t3335150@naver.com>
2025-09-17 22:51:45 +0900

    feat: 개별 일기 아이템 컴포넌트 추가
    - DiaryItem.jsx: 일기 목록에서 표시되는 개별 일기
    - DiaryItem.css: 일기 아이템 스타일링

---
+++ emotion-diary/src/components/DiaryItem.jsx
@@ -0,0 +1,33 @@
+import "./DiaryItem.css";
+import getEmotionImage from "../util/get-emotion-image";
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
+
+const DiaryItem = ({ id, createdDate, emotionId, content }) => {
+  const nav = useNavigate();
+  return (
+    <div className="DiaryItem">
+      <div
+        className={`img_section img_section_${emotionId}`}
+        onClick={() => nav(`/diary/${id}`)}
+      >
+        <img src={getEmotionImage(emotionId)} />
+      </div>
+      <div className="info_section" onClick={() => nav(`/diary/${id}`)}>
+        <div className="created_date">
+          {new Date(createdDate).toLocaleDateString()}
+        </div>
+        <div className="content">{content}</div>
+      </div>
+      <div className="button_section">
+        <Button
+          text="수정하기"
+          type="DEFAULT"
+          onClick={() => nav(`/edit/${id}`)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DiaryItem;
